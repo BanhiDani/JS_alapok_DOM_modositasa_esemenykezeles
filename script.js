c3566412223ec8c754e2fadaf76ddac98638b72d
BanhiDani <sistbroo@gmail.com>
2026-02-26 13:35:06 +0100

    felig kesz

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-   
+    const lista = [];
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
+  elemekElerese4();
+  elemekFormazasa1();
 });
 
 function elemekElerese1() {
-   
+  let elem = document.querySelector("section h2");
+  console.log(elem.innerHTML);
 }
 function elemekElerese2() {
-   
+  let elem = document.getElementById("ide");
+  elem.innerHTML = "<p>Jó reggelt!</p>";
+}
+
+function elemekElerese3() {
+  const elem = document.getElementsByClassName("ide");
+  elem[0].innerHTML = "<p>Jó reggelt!</p>";
+  console.log(elem);
+}
+
+function elemekElerese4() {
+  let elem = document.getElementsByClassName("lista");
+ 
+  elem[0].innerHTML = "<ul>";
+  
+  for (let index = 0; index < 5; index++) {
+    let rndSzam = Math.random() * 21 + 10;
+    rndSzam = Math.floor(rndSzam);
+    console.log(rndSzam);
+    elem[0].innerHTML += `<li>${rndSzam}</li>`;
     
+  }
+  elem[0].innerHTML += "<ul>";
+}
+
+function elemekFormazasa1() {
+  let elem = document.getElementsByClassName("lista");
+  elem[0].classList.add("formazott");
+  elem[0].addEventListener("click", elemekFormazasa2);
+}
+
+function elemekFormazasa2() {
+  let elem = document.getElementsByClassName("kattintasutan")
+  let elem2 = document.getElementsByClassName("lista")
+    elem[0].innerHTML = elem2[0].innerHTML;
 }
